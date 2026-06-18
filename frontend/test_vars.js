e36b966706fda48e13aafd429ad39bd1cf6eddfe
Eugeniu Cazmal <jeka7ro@gmail.com>
2026-06-18 10:53:36 +0300

    security: remove debug scripts with exposed credentials

--- frontend/test_vars.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/pages/employee/ClockInPage.jsx', 'utf8');
-
-// Find all words that look like function calls or variables, check if they are defined
-const matches = [...content.matchAll(/\b([a-zA-Z_]\w*)\b/g)].map(m => m[1]);
-
-console.log("File read successfully, length: ", content.length);
